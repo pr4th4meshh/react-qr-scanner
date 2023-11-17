a89ab5bfb073bcdaf4c017949e483f41f0898c6b
Prathamesh Asolkar <prathameshasolkar17@gmail.com>
2023-11-17 20:02:58 +0530

    fixed qrs

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import { Html5QrcodeScanner } from 'html5-qrcode'
+import { useEffect, useState } from "react"
+import "./App.css"
+import { Html5QrcodeScanner } from "html5-qrcode"
 
 function App() {
   const [scanResult, setScanResult] = useState(null)
-  useEffect(()=> {
-    const scanner = new Html5QrcodeScanner('reader', {
-      qrbox : {
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner("reader", {
+      qrbox: {
         width: 250,
-        height: 250
+        height: 250,
       },
       fps: 5,
-      
-    })
-  
-    scanner.render(success, error);
+      rememberLastUsedCamera: true,
+    },true)
+
+    scanner.render(success, error)
     function success(result) {
-      scanner.clear();
+      scanner.clear()
       setScanResult(result)
     }
-  
+
     function error(err) {
       console.warn(err)
     }
   }, [])
   return (
-    <div className='App'>
-    {
-      scanResult ?
-      <div>Scsds: <a href={scanResult}>Click this link to navigate to: </a>{scanResult}</div> :
-      <div id='reader'></div>
-    }
+    <div className="App">
+      {scanResult ? (
+        <div>
+          Scsds: <a href={scanResult}>Click this link to navigate to: </a>
+          {scanResult}
+        </div>
+      ) : (
+        <div id="reader"></div>
+      )}
     </div>
   )
 }
